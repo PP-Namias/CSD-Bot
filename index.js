69212173b2b265b6bde403c8332889cb15d71234
Jhon Keneth Namias <75213409+PP-Namias@users.noreply.github.com>
2022-11-30 10:42:09 +0800

    v1.02

--- index.js
+++ index.js
@@ -1,29 +1,58 @@
-const { CommandClient } = require('eris')
+const Discord = require("discord.js")
+require("dotenv").config()
 
-// Stupid ass bot creation
-async function init(token) {
-    const CSDBot = new CommandClient(`Bot ${token}`, { intents: ['guilds'], maxShards: 'auto',restMode: true })
-    // Register the stupid ass command
-    CSDBot.on('ready', async () => {
-        await CSDBot.bulkEditCommands([{
-            name: 'lol',
-            description: 'I hate discord so much you cannot believe it',
-            type: 1,
-        }])
-        console.log(`Paste the URL below into your browser to invite your bot!\nhttps://discord.com/oauth2/authorize?client_id=${CSDBot.user.id}&scope=applications.commands%20bot&permissions=3072`)
-    })
-    // Stupid ass interaction creation event
-    CSDBot.on('interactionCreate', async (interaction) => {
-        if (interaction?.data?.name === 'lol') {
-            await interaction.createMessage({
-                content: 'CSD Bot working lmao'
-            })
-            console.log('Self destructing...')
-            process.exit(0)
-        }
-    })
-    CSDBot.connect();
+// const generateImage = require("./generateImage")
+
+const client = new Discord.Client({
+    intents: [
+        "GUILDS",
+        "GUILD_MESSAGES",
+        "GUILD_MEMBERS"
+    ]
+})
+
+let bot = {
+    client, 
+    prefix: "n.",
+    owners: ["315850603396071424"]
 }
 
-const tokenFromStupidCommand = process.argv[2]
-init(tokenFromStupidCommand);
+client.commands = new Discord.Collection()
+client.events = new Discord.Collection()
+client.slashcommands = new Discord.Collection()
+client.buttons = new Discord.Collection()
+
+client.loadEvents = (bot, reload) => require("./handlers/events")(bot, reload)
+client.loadCommands = (bot, reload) => require("./handlers/commands")(bot, reload)
+client.loadSlashCommands = (bot, reload) => require("./handlers/slashcommands")(bot, reload)
+client.loadButtons = (bot, reload) => require("./handlers/buttons")(bot, reload)
+
+client.loadEvents(bot, false)
+client.loadCommands(bot, false)
+client.loadSlashCommands(bot, false)
+client.loadButtons(bot, false)
+
+
+module.exports = bot
+
+// client.on("ready", () => {
+//     console.log(`Logged in as ${client.user.tag}`)
+// })
+
+// client.on("messageCreate", (message) => {
+//     if (message.content == "hi"){
+//         message.reply("Hello World!")
+//     }
+// })
+
+// const welcomeChannelId = "926530810008453120"
+
+// client.on("guildMemberAdd", async (member) => {
+//     const img = await generateImage(member)
+//     member.guild.channels.cache.get(welcomeChannelId).send({
+//         content: `<@${member.id}> Welcome to the server!`,
+//         files: [img]
+//     })
+// })
+
+client.login(process.env.TOKEN)
