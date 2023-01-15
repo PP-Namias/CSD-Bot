2b538bd0613489e616f926722f2c58a94cdd2555
Jhon Keneth Namias <75213409+PP-Namias@users.noreply.github.com>
2023-01-15 08:45:57 +0800

    :sparkles: Added Triggers Schedule :sparkles:
    there still bugs on the user mentioning part but i'll solve it shortly :+1:

--- index.js
+++ index.js
@@ -51,6 +51,43 @@ client.on("messageCreate", (message) => {
     }
 })
 
+client.on("messageCreate", (message) => {
+    if (message.content == "cs1asched"){
+        message.reply("https://cdn.discordapp.com/attachments/1018057172711653437/1063708350073806858/CS1A_Sched.png")
+    }
+})
+
+client.on("messageCreate", (message) => {
+    if (message.content == "is1asched"){
+        message.reply("https://media.discordapp.net/attachments/1008642078592471084/1034046317158215751/issched.png?width=610&height=597")
+    }
+})
+
+client.on("messageCreate", (message) => {
+    if (message.content == "it1asched"){
+        message.reply("https://media.discordapp.net/attachments/1034063578333990962/1034071467832389703/BSIT_1A_NORTH.jpg?width=379&height=490")
+    }
+})
+
+client.on("messageCreate", (message) => {
+    if (message.content == "vods 1st sem"){
+        message.reply("https://youtube.com/playlist?list=PL-EDcGHq7b9wUdGrWmFjD7vtCxNmDePdC")
+    }
+})
+
+// When I started writing this, only God and I understood what I was doing
+// Now, only God knows
+client.on("messageCreate", (message) => {
+    if (message.content == "r1"){
+        message.reply("<@&1011206648129409107>, ${user} is recording...")
+        message.mentions.members
+        message.mentions.members.first().toString()
+        message.reply("<@&1011206648129409107>, ${person.user} is recording...")
+        message.guild.members.cache.get() 
+        `<@${member.id}>`
+    }
+})
+
 // const welcomeChannelId = "1052346489017466900"
 
 // client.on("guildMemberAdd", async (member) => {
